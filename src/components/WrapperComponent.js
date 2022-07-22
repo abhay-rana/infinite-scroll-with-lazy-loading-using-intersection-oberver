697ed264a8df6512f2546344a913ba8c599dd6d9
Abhay Rana <abhay.rana@healthmug.com>
2022-07-22 22:39:38 +0530

    wrapper component

--- src/components/WrapperComponent.js
+++ src/components/WrapperComponent.js
@@ -2,7 +2,7 @@ import React, { useCallback, useMemo, useState, memo, useEffect } from "react";
 
 const WrapperComponent = () => {
 	const [state, setState] = useState(0);
-	const render = useCallback(() => <Child />, [state]);
+	const render = useCallback(() => <Child />, [state]); // on every re-rendering render function  make a new reference and when react reconcile and compare the render function reference so the old reference is deleted and so then the child component is unmounted
 
 	return (
 		<>
@@ -15,8 +15,6 @@ const WrapperComponent = () => {
 };
 
 const Child = (props) => {
-	const [state, setState] = useState(0);
-
 	useEffect(() => {
 		console.log("mounts child component");
 		return () => {
@@ -37,7 +35,7 @@ const Parent = memo((props) => {
 	return (
 		<>
 			<div>Parent Component</div>
-			<button onClick={() => setState(state + 1)} classname="cursor-pointer p-2 bg-red-500">
+			<button onClick={() => setState(state + 1)} className="cursor-pointer p-2 bg-red-500">
 				Change Child Count
 			</button>
 			<props.child />
